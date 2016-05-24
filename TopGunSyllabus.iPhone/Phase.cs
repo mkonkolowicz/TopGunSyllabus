2c06c40c6d238fa5874cf0dc01eb99150a6f57fc
Maciej Konkolowicz <mkonkolowicz@gmail.com>
2016-05-23 20:08:25 -0400

    Added splash screen images

---
+++ TopGunSyllabus.iPhone/Phase.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace TopGunSyllabus.iPhone
+{
+	partial class Phase : UIViewController
+	{
+		public Phase (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
---
+++ TopGunSyllabus.iPhone/Phase.designer.cs
@@ -0,0 +1,53 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace TopGunSyllabus.iPhone
+{
+	[Register ("Phase")]
+	partial class Phase
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView Banner { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextView PhaseDetails { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextView PhaseSummary { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel TopGunSyllabusLabel { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (Banner != null) {
+				Banner.Dispose ();
+				Banner = null;
+			}
+			if (PhaseDetails != null) {
+				PhaseDetails.Dispose ();
+				PhaseDetails = null;
+			}
+			if (PhaseSummary != null) {
+				PhaseSummary.Dispose ();
+				PhaseSummary = null;
+			}
+			if (TopGunSyllabusLabel != null) {
+				TopGunSyllabusLabel.Dispose ();
+				TopGunSyllabusLabel = null;
+			}
+		}
+	}
+}
