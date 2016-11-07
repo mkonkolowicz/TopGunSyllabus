7d5560a7c0df17cac0ee1c63e1e3c3f5974d465d
Maciej Konkolowicz <mkonkolowicz@gmail.com>
2016-11-07 15:51:52 -0500

    * Main.storyboard: Added class name and accessibility id to phases   link on first controller
    * UIPhases.cs:
    * UIPhases.designer.cs:
    * Resource.Designer.cs:
    * TopGunSyllabus.iPhone.csproj:

---
+++ TopGunSyllabus.iPhone/UIPhases.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace TopGunSyllabus.iPhone
+{
+    public partial class UIPhases : UIButton
+    {
+        public UIPhases (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
